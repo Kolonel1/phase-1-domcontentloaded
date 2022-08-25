3c6bdbd241c0303e70de0113e47a8759756e19f4
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-26 00:16:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded',()=>{
+   let p = document.getElementById('text')
+   console.log(p)
+   p.textContent = "This is really cool!"
+
+} )
